4bf741e6342e53b6545f479a7e383a3c97ceab2f
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-09-10 23:38:28 +0100

    modified settings file for debugging

--- dokita/dokita/settings.py
+++ dokita/dokita/settings.py
@@ -19,14 +19,9 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-if DEBUG:
-    ALLOWED_HOSTS = ['.vercel.app', '.now.sh' ]
-    
-else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]
-    
 
-# ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
